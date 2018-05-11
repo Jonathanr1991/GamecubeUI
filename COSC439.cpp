0a60a4580c9f9ed1a29fa9664c5b8d09531ba807
pjasinski <pjasin1@students.towson.edu>
2018-05-11 15:25:20 -0400

    we have lift off boys

--- COSC439.cpp
+++ COSC439.cpp
@@ -1,22 +1,21 @@
 #include <SDL2/SDL.h>
-#include "SDL.h"
 #include <iostream>
 #include <vector>
 #include "SDL_keyboard.h"
 #include "SDL_keycode.h"
 #include "SDL_scancode.h"
-//#include "Sprite.h"
+#include "Sprite.h"
+//#include "controller.h"
 #include <SDL_gamecontroller.h>
+
+#define BMPSIZE 240
+#define LEFT_DEADZONE -8000
+#define RIGHT_DEADZONE 8000
 
 #define ADVANCE_LEFT -1
 #define ADVANCE_RIGHT 1
 #define ADVANCE_DOWN 2
 #define ADVANCE_UP 2
-
-#define RIGHT_DEADZONE 8000
-#define LEFT_DEADZONE -8000
-#define DOWN_DEADZONE 8000
-#define UP_DEADZONE -8000
 
 #define SMASH "Smash Bros"
 #define SONICHEROES "Sonic Heroes"
@@ -25,42 +24,9 @@
 #define fps 60
 using namespace std;
 
-class Sprite{
-
-private:
-
-  SDL_Rect rect;
-  int origin_x, origin_y;
-  SDL_Surface *image;
-public:
-
-  Sprite(Uint32 color,int x,int y, int w, int h)  {
-    image = SDL_CreateRGBSurface(0,w,h,32,0,0,0,0);
-
-    SDL_FillRect(image, NULL, color);
-    rect= image->clip_rect;
-    origin_x=0;
-    origin_y=0;
-    rect.x=x- origin_x;
-    rect.y=y-origin_y;
-
-  }
-
-  void update(){
-  }
-		
-  void draw(SDL_Surface *destination){			
-    SDL_BlitSurface(image,NULL,destination, &rect);
-  }
-								
-		
-		
-};
-
-/*
-void log(char* string) {
-  fprintf(f,string);
-  }*/
+void printRect(SDL_Rect *r) {
+  printf("x: %d y: %d, w: %d, h: %d\n", r->x, r->y, r->w, r->h);
+}
 
 void executeDolphin(string game) { // TODO put macros here
   if (game.compare("Smash Bros") == 0)
@@ -101,7 +67,7 @@ int main( int argc, char *argv[]){
 			     SDL_WINDOWPOS_UNDEFINED,
 			     SDL_WINDOWPOS_UNDEFINED,
 			     SCREEN_WIDTH, 
-			     SCREEN_HEIGHT,       SDL_WINDOW_RESIZABLE);
+			     SCREEN_HEIGHT,       SDL_WINDOW_FULLSCREEN);
     if (window!= NULL)
       printf("created window\n");
     
@@ -111,118 +77,59 @@ int main( int argc, char *argv[]){
       exit(1);
     }
 
-    printf("inited renderer\n");
-    SDL_Surface *game1,*game2, *game3=NULL;
-    SDL_Surface *gameCubeLogo, *gameSelector=NULL;
-    Uint32 blue = SDL_MapRGB(screen->format,0,0,150);
-    Uint32 blue2 = SDL_MapRGB(screen->format,0,0,255);
-    Uint32 white = SDL_MapRGB(screen->format,255,255,255);
-    printf("made colors and surfaces\n");
-    //main background color
-    SDL_FillRect(screen, NULL, blue2);
-    printf("filled background\n");
-    //Game 1 location main menu
-    SDL_Rect box;
-    box.x=(2*SCREEN_WIDTH)/32;
-    box.y=(6*SCREEN_HEIGHT)/18;
-    printf("made game1 box\n");
-    // game 2 location
-    SDL_Rect box2;
-    box2.x=(10*SCREEN_WIDTH)/32;
-    box2.y=box.y;
-    printf("made game2 box\n");
-    // game 3 location
-    SDL_Rect box3;
-    box3.x=(10*SCREEN_WIDTH)/32 +320;
-    box3.y=box.y;
-    printf("made game3 box\n");
-    SDL_Rect logoLocation;
-    logoLocation.x=(10*SCREEN_WIDTH)/32;
-    logoLocation.y=10;
-    printf("made logo box\n");
+    //    SDL_Rect box1, box2, box3, box4, logoRect; // rects we're gonna render to
+    SDL_Rect box1 = { (2*SCREEN_WIDTH)/32, (6*SCREEN_HEIGHT)/18, BMPSIZE, BMPSIZE};
+    SDL_Rect box2 = { (10*SCREEN_WIDTH)/32, (6*SCREEN_HEIGHT)/18, BMPSIZE, BMPSIZE };
+    SDL_Rect box3 = { ((10*SCREEN_WIDTH) / 32)+320, (6*SCREEN_HEIGHT)/18, BMPSIZE, BMPSIZE};
+    SDL_Rect logoRect = { (10*SCREEN_WIDTH)/32, 10, 768, 176 };
+    SDL_Rect *gameSelectorLocation = &box1;
     
-    // set the color for clearing the renderer to the background color
-    // Pictures for game menu set up
-    game1=SDL_LoadBMP("img/sunshine.bmp");
-    game2=SDL_LoadBMP("img/smash.bmp");
-    game3=SDL_LoadBMP("img/sonicheroes.bmp");
-    gameCubeLogo=SDL_LoadBMP("img/GamecubeLogo.bmp");
-    gameSelector= SDL_LoadBMP("img/gameSelectorPic.bmp");
-    printf("inited BMPs\n");
-
-    // create sprites
-    Sprite topBox( blue,0,0, SCREEN_WIDTH,3*SCREEN_HEIGHT/18);
-    Sprite bottomBox(blue,0,SCREEN_HEIGHT-SCREEN_HEIGHT/6,SCREEN_WIDTH, SCREEN_HEIGHT/6); 
-    bottomBox.draw(screen);
-    Sprite pressAToStartGame(white, (11*SCREEN_WIDTH)/32, (16*SCREEN_HEIGHT)/18, (10*SCREEN_WIDTH)/32, (SCREEN_HEIGHT)/18);
-    pressAToStartGame.draw(screen);
-    printf("created Sprites\n");
-
+    printf("box1: ");
+    printRect(&box1);
+    printf("box2: ");
+    printRect(&box2);
+    printf("box3: ");
+    printRect(&box3);
+    printf("logo: ");
+    printRect(&logoRect);
     
 
+    // surfaces we're gonnna create the textures with
+    SDL_Surface *game1s;
+    SDL_Surface *game2s;
+    SDL_Surface *game3s;
+    SDL_Surface  *logos;
+    SDL_Surface *gameSelectors;
     
-    // GAME SELECTOR
-    SDL_Rect gameSelectorLocation;
-    SDL_Rect *gameSelectorLocation_p = &gameSelectorLocation;
-    // set location of the rect
-    gameSelectorLocation.x=(2*SCREEN_WIDTH)/32-10;
-    gameSelectorLocation.y = (6*SCREEN_HEIGHT)/18-10;
-    printf("inited gameSelector rect\n");
-    // init the texture for the selector icon
+    game1s = SDL_LoadBMP("res/sunshine.bmp");
+    game2s = SDL_LoadBMP("res/melee.bmp");
+    game3s = SDL_LoadBMP("res/sonicheroes.bmp");
+    logos = SDL_LoadBMP("res/gamecubelogo.bmp");
+    gameSelectors = SDL_LoadBMP("res/selector.bmp");
     
-    // blit static BMPs
-
-    SDL_BlitSurface(gameCubeLogo,NULL,screen,&logoLocation);
-    printf("blitted logo\n");    
-    SDL_BlitSurface(game1,NULL,screen,&box);
-    printf("blitted game1\n");
-    SDL_BlitSurface(game2,NULL,screen,&box2);
-    printf("blittled game2\n");
-    SDL_BlitSurface(game3,NULL,screen,&box3);
-    printf("blittled game 3\n");
-    
-    SDL_UpdateWindowSurface(window);
-    printf("updated window surface");
-    //    printf("updated window surface\n");
-    // render the selector at the game 1 rect.
     SDL_Renderer *renderer;
-
-    renderer = SDL_CreateRenderer(window, -1, 0);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // TODO figure out how to put blue2 here.
-    printf("set renderer draw color\n");
-    
-    SDL_Texture *gameSelectorTexture = NULL;
-    gameSelectorTexture = SDL_CreateTextureFromSurface(renderer, gameSelector);
-    printf("created texture from surface\n");
-    SDL_FreeSurface(gameSelector);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_SetRenderDrawColor(renderer, 0,0,255,255);
     SDL_RenderClear(renderer);
-    printf("cleared renderer\n");
-    SDL_RenderCopy(renderer, gameSelectorTexture, NULL, &box);
-    printf("called rendercopy\n");
-    SDL_RenderPresent(renderer);
-    printf("called renderpresent\n");
-    gameSelectorLocation_p = &box;
-    printf( "rendered selector initially\n");
+    SDL_Texture *game1, *game2, *game3, *logo, *gameSelector;
+    game1 = SDL_CreateTextureFromSurface(renderer, game1s);
+    game2 = SDL_CreateTextureFromSurface(renderer, game2s);
+    game3 = SDL_CreateTextureFromSurface(renderer, game3s);
+    logo = SDL_CreateTextureFromSurface(renderer, logos);
+    gameSelector = SDL_CreateTextureFromSurface(renderer, gameSelectors);
     
-    SDL_GameController *controller;
-    printf("controller created\n");
+    SDL_FreeSurface(game1s);
+    SDL_FreeSurface(game2s);
+    SDL_FreeSurface(game3s);
+    SDL_FreeSurface(logos);
+    SDL_FreeSurface(gameSelectors);
+    // render games
 
-    for (int i = 0; i < SDL_NumJoysticks(); i++) {
-      if (SDL_IsGameController(i)) {
-	controller=SDL_GameControllerOpen(i);
-	printf("found controller\n");
-	break;
-      }
-    }
-    // check to see if theres at least one controller 
-    if (controller == NULL) {
-      printf("No controllers exiting\n");
-      exit(1);
-    }
-    // if there isn't a controller plugged in, do something about it
-    // TODO make it do something so that a user can just plug a controller in.
-    // run an idle loop.
-
+    SDL_RenderCopy(renderer, game1, NULL, &box1);
+    SDL_RenderCopy(renderer, game2, NULL, &box2);
+    SDL_RenderCopy(renderer, game3, NULL, &box3);
+    SDL_RenderCopy(renderer, logo, NULL, &logoRect);
+    SDL_RenderPresent(renderer);
     
     SDL_Event event;
     bool running = true;
@@ -239,31 +146,31 @@ int main( int argc, char *argv[]){
 			    printf("event axis motion\n");
 				  if (event.caxis.which == 0) { // TODO MAKE SURE THIS WORKS WITH THE RIGHT AXIS!!!!!!!!!! IS THIS THE LEFT/RIGHT OR UP/DOWN ONE?
 					  if (event.caxis.value < LEFT_DEADZONE) {
-						  if (SDL_RectEquals(gameSelectorLocation_p, &box) == SDL_TRUE) { // super mario sunshine is selected
+						  if (SDL_RectEquals(gameSelectorLocation, &box1) == SDL_TRUE) { // super mario sunshine is selected
 							  if (event.caxis.value < RIGHT_DEADZONE) {
-								  advanceSelection(renderer, gameSelectorTexture, &box, &box2);
-								  gameSelectorLocation_p = &box2;
+								  advanceSelection(renderer, gameSelector, &box1, &box2);
+								  gameSelectorLocation = &box2;
 								  printf("box1: moving right: %d\n", event.caxis.value);
 							  }
 						  }
 
-						  if (SDL_RectEquals(gameSelectorLocation_p, &box2) == SDL_TRUE) { // smash is selected
+						  if (SDL_RectEquals(gameSelectorLocation, &box2) == SDL_TRUE) { // smash is selected
 							  if (event.caxis.value < LEFT_DEADZONE) {
-								  advanceSelection(renderer, gameSelectorTexture, &box2, &box);
-								  gameSelectorLocation_p = &box;
+								  advanceSelection(renderer, gameSelector, &box2, &box1);
+								  gameSelectorLocation = &box1;
 								  printf("box2: moving left: %d\n", event.caxis.value);
 							  }
 							  else if (event.caxis.value < RIGHT_DEADZONE) {
-								  advanceSelection(renderer, gameSelectorTexture, &box2, &box3);
-								  gameSelectorLocation_p = &box3;
+								  advanceSelection(renderer, gameSelector, &box2, &box3);
+								  gameSelectorLocation = &box3;
 								  printf("box2: moving right: %d\n", event.caxis.value);
 							  }
 						  }
 
-						  if (SDL_RectEquals(gameSelectorLocation_p, &box3) == SDL_TRUE) { // sonic heroes is selected.
+						  if (SDL_RectEquals(gameSelectorLocation, &box3) == SDL_TRUE) { // sonic heroes is selected.
 							  if(event.caxis.value < LEFT_DEADZONE) {
-								  advanceSelection(renderer, gameSelectorTexture, &box3, &box2);
-								  gameSelectorLocation_p = &box2;
+								  advanceSelection(renderer, gameSelector, &box3, &box2);
+								  gameSelectorLocation = &box2;
 								  printf("box3: moving left: %d\n", event.caxis.value);
 							  }
 						  }
@@ -273,11 +180,11 @@ int main( int argc, char *argv[]){
 
 			  if (event.type == SDL_CONTROLLERBUTTONDOWN) {
 				  if (event.cbutton.button == SDL_CONTROLLER_BUTTON_A) {
-					  if (SDL_RectEquals(gameSelectorLocation_p, &box) == SDL_TRUE)
+					  if (SDL_RectEquals(gameSelectorLocation, &box1) == SDL_TRUE)
 						  executeDolphin(SUNSHINE);
-					  if (SDL_RectEquals(gameSelectorLocation_p, &box2) == SDL_TRUE)
+					  if (SDL_RectEquals(gameSelectorLocation, &box2) == SDL_TRUE)
 						  executeDolphin(SMASH);
-					  if (SDL_RectEquals(gameSelectorLocation_p, &box3) == SDL_TRUE)
+					  if (SDL_RectEquals(gameSelectorLocation, &box3) == SDL_TRUE)
 						  executeDolphin(SONICHEROES);
 				  }
 
--- textures.cpp
+++ textures.cpp
@@ -1,5 +1,8 @@
 #include "Sprite.h"
 #include <string.h>
+void printRect(SDL_Rect *r) {
+  printf("x: %d y: %d, w: %d, h: %d\n", r->x, r->y, r->w, r->h);
+}
 void createTexture(SDL_Renderer *r, SDL_Texture *t, SDL_Surface *s, string imgPath) {
   s = SDL_LoadBMP(imgPath);
   t = SDL_LoadTextureFromSurface(r,s);
